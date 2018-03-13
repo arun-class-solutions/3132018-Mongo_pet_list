5c69793d69f1ee3f7c64921bde074c28a65bf819
Arun Sood <arsood@gmail.com>
2018-03-13 15:03:02 -0700

    Show all owners

---
+++ resources/owners/controllers/index.js
@@ -0,0 +1,29 @@
+// MongoDB connection
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const mongoUrl = "mongodb://localhost:27017/pet_list_db";
+
+module.exports = (req, res) => {
+  // Find all owners from the "owners" collection here
+  // Aggregate them using the $lookup operator to join their associated pets
+  // Call the method below, passing in the owners array:
+
+  // res.render("index", {
+  //   owners: ownersArray
+  // });
+
+  // Navigate to http://localhost:3000 to see UI
+
+  MongoClient.connect(mongoUrl, (err, db) => {
+
+    db
+    .collection("owners")
+    .find({})
+    .toArray((err, ownersArray) => {
+      res.render("index", {
+        owners: ownersArray
+      });
+    });
+
+  });
+}
